c2c3ffaa668506008620a8cfd71b22a84d2b53f4
ffthylmz <ffthylmz@outlook.com>
2022-08-20 15:07:11 +0300

    Add project files.

---
+++ WFAEntity200822G/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace WFAEntity200822G
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.btnGuncelle = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.btnListele = new System.Windows.Forms.Button();
+            this.btnId = new System.Windows.Forms.Button();
+            this.btnListe = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(507, 443);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(581, 28);
+            this.txtId.Multiline = true;
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(222, 34);
+            this.txtId.TabIndex = 1;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(581, 88);
+            this.txtName.Multiline = true;
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(222, 34);
+            this.txtName.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(547, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "ID:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(532, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Name:";
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnEkle.Location = new System.Drawing.Point(681, 146);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(122, 44);
+            this.btnEkle.TabIndex = 5;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // btnGuncelle
+            // 
+            this.btnGuncelle.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnGuncelle.Location = new System.Drawing.Point(543, 213);
+            this.btnGuncelle.Name = "btnGuncelle";
+            this.btnGuncelle.Size = new System.Drawing.Size(122, 44);
+            this.btnGuncelle.TabIndex = 6;
+            this.btnGuncelle.Text = "Güncelle";
+            this.btnGuncelle.UseVisualStyleBackColor = true;
+            this.btnGuncelle.Click += new System.EventHandler(this.btnGuncelle_Click);
+            // 
+            // btnSil
+            // 
+            this.btnSil.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnSil.Location = new System.Drawing.Point(681, 213);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(122, 44);
+            this.btnSil.TabIndex = 7;
+            this.btnSil.Text = "Sil";
+            this.btnSil.UseVisualStyleBackColor = true;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
+            // 
+            // btnListele
+            // 
+            this.btnListele.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnListele.Location = new System.Drawing.Point(543, 146);
+            this.btnListele.Name = "btnListele";
+            this.btnListele.Size = new System.Drawing.Size(122, 44);
+            this.btnListele.TabIndex = 8;
+            this.btnListele.Text = "Bul";
+            this.btnListele.UseVisualStyleBackColor = true;
+            this.btnListele.Click += new System.EventHandler(this.btnListele_Click);
+            // 
+            // btnId
+            // 
+            this.btnId.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnId.Location = new System.Drawing.Point(614, 282);
+            this.btnId.Name = "btnId";
+            this.btnId.Size = new System.Drawing.Size(122, 44);
+            this.btnId.TabIndex = 9;
+            this.btnId.Text = "ID\'ye Göre Bul";
+            this.btnId.UseVisualStyleBackColor = true;
+            this.btnId.Click += new System.EventHandler(this.btnId_Click);
+            // 
+            // btnListe
+            // 
+            this.btnListe.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnListe.Location = new System.Drawing.Point(614, 411);
+            this.btnListe.Name = "btnListe";
+            this.btnListe.Size = new System.Drawing.Size(122, 44);
+            this.btnListe.TabIndex = 10;
+            this.btnListe.Text = "Listele";
+            this.btnListe.UseVisualStyleBackColor = true;
+            this.btnListe.Click += new System.EventHandler(this.btnListe_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(831, 467);
+            this.Controls.Add(this.btnListe);
+            this.Controls.Add(this.btnId);
+            this.Controls.Add(this.btnListele);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.btnGuncelle);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Button btnGuncelle;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.Button btnListele;
+        private System.Windows.Forms.Button btnId;
+        private System.Windows.Forms.Button btnListe;
+    }
+}
+
---
+++ WFAEntity200822G/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFAEntity200822G
+{
+
+    public partial class Form1 : Form
+    {
+        public void DgvDoldur()
+        {
+            dataGridView1.DataSource = entities.Customers.ToList();
+            dataGridView1.ClearSelection();
+            txtId.Clear();
+            txtName.Clear();
+        }
+
+        NorthwindEntities1 entities = new NorthwindEntities1();
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        //ID'ye ve AD'a göre silme işlemi...
+
+        private void btnListele_Click(object sender, EventArgs e)
+        {
+            Customer customer = new Customer();
+            customer.CustomerID = txtId.Text;
+            customer.ContactName = txtName.Text;
+
+            dataGridView1.DataSource = entities.Customers.Where(s => s.CustomerID == customer.CustomerID && s.ContactName == customer.ContactName).ToList();            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        //ID'ye göre bulma işlemi...
+        private void btnId_Click(object sender, EventArgs e)
+        {
+            Customer customer = new Customer();
+            customer.CustomerID = txtId.Text;
+
+            dataGridView1.DataSource = entities.Customers.Where(s => s.CustomerID == customer.CustomerID).ToList();           
+        }
+
+        private void btnListe_Click(object sender, EventArgs e)
+        {
+            DgvDoldur();
+        }
+
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            Customer customer = new Customer();
+            customer.CustomerID = txtId.Text;
+            customer.CompanyName = txtName.Text;
+
+            entities.Customers.Add(customer);
+            entities.SaveChanges();
+
+            MessageBox.Show(customer.CustomerID + " - " + customer.CompanyName + " isimli kayıt oluşturulmuştur.");
+
+            DgvDoldur();
+        }
+
+        //ID'ye göre güncelleme işlemi...
+
+        private void btnGuncelle_Click(object sender, EventArgs e)
+        {
+            Customer customer = entities.Customers.Find(txtId.Text);
+            if (customer != null)
+            {
+                customer.CompanyName = txtName.Text;
+                entities.SaveChanges();
+                MessageBox.Show(customer.CustomerID + " ID'li kayıt güncellenmiştir.");
+            }
+            else
+                MessageBox.Show(txtId.Text + " ilgili kayıt bulunamadı.");
+
+            DgvDoldur();
+        }
+
+        //ID'ye göre silme işlemi...
+
+        private void btnSil_Click(object sender, EventArgs e)
+        {
+            Customer customer = entities.Customers.Find(txtId.Text);
+            if (customer != null)
+            {
+                entities.Customers.Remove(customer);
+                entities.SaveChanges();
+                MessageBox.Show(txtId.Text + " ID'li veri silinmiştir.");
+
+            }
+
+            DgvDoldur();
+        }
+    }
+}
